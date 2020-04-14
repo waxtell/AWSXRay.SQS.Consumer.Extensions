0cf1d7b108b9b88dd4e40d29e3a95652a25f64a3
Will Axtell <waxtell@transtar1.com>
2020-04-14 15:57:25 -0400

    Divested from existing project for packaging purposes.

---
+++ AWSXRay.SQS.Consumer.Extensions/MessageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.SQS.Model;
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+using AWSXRay.SQS.Consumer.Extensions.Metadata;
+
+namespace AWSXRay.SQS.Consumer.Extensions
+{
+    public static class MessageExtensions
+    {
+        public static TraceHeader GetTraceHeader(this Message message)
+        {
+            return 
+                message.Attributes.TryGetValue(Constants.TraceHeader, out var value) 
+                    ? TraceHeader.FromString(value) 
+                    : null;
+        }
+    }
+}
---
+++ AWSXRay.SQS.Consumer.Extensions/ReceiveMessageRequestExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Amazon.SQS.Model;
+using AWSXRay.SQS.Consumer.Extensions.Metadata;
+
+namespace AWSXRay.SQS.Consumer.Extensions
+{
+    public static class ReceiveMessageRequestExtensions
+    {
+        public static ReceiveMessageRequest WithTraceHeader(this ReceiveMessageRequest request)
+        {
+            if (request.AttributeNames == null)
+            {
+                request.AttributeNames = new List<string>();
+            }
+
+            if (!request.AttributeNames.Contains(Constants.TraceHeader))
+            {
+                request.AttributeNames.Add(Constants.TraceHeader);
+            }
+
+            return request;
+        }
+    }
+}
